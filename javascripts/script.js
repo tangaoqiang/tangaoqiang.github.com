305a950a5a34984db69ece2be210b736bbcbfa93
tangaoqiang <479682852@qq.com>
2016-09-08 17:28:24 +0800

    gai

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,11 +1,23 @@
 'use strict';
 $(function(){
     //鼠标移入微信
-    $('.nav_top i').bind('mouseover',function(){
-        $('.nav_top i .code').css({top:0});
+    $('.nav_top .codeBox').bind('mouseover',function() {
+        $('.nav_top .codeBox .code').stop().animate({
+            "top": 36
+        });
     });
     //鼠标移出微信
-    $('.nav_top i').bind('mouseout',function(){
-        $('.nav_top i .code').css({top:-100});
+    $('.nav_top .codeBox').bind('mouseout',function() {
+        $('.nav_top .codeBox .code').stop().animate({
+            "top": -100
+        });
     });
+    //导航条开始
+    var aL = $('.nav .nav_right li');
+    aL.each(function(index,element){
+        $(this).bind('mouseover',function(){
+
+        })
+    });
+
 });
